e41ce3ed76b7a108932247ba59f5c8fc352480d8
liuyuhang1 <liuyuhang1@lixiang.com>
2026-02-09 14:10:44 +0800

    feat: add

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, ipcMain} = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+    
+    // 在开发模式下自动打开开发者工具
+    if (process.env.NODE_ENV === 'development') {
+        win.webContents.openDevTools()
+    }
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
